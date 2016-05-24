fe990a035c98dd06ade79502d9ed5dd0a750abfc
Valerii <Valer4eg209@gmail.com>
2016-05-24 16:28:42 +0300

    Create MyList.py

---
+++ MyList.py
@@ -0,0 +1,14 @@
+class MyList():
+    def __init__(self, l=[]):
+        self._l = l.copy()
+    def __repr__(self):
+        return repr(self._l)
+    def __len__(self):
+        return len(self._l)
+    def __add__(self, value):
+        self._l.append(value)
+    def __setitem__(self, index, value):
+        self._l[index] = value
+    def __getitem__(self, index):
+        return self._l[index]
+    
