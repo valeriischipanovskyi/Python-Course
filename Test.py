ffab2a8f193c0dff10fe8b2aad38eefd0f87169e
Valerii <Valer4eg209@gmail.com>
2016-05-24 11:53:16 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,33 +6,57 @@ Delete
 """
 phone_book = {}
 
-while True:
-    print("Make your choice:\n1. Create\n2. Read\n3. Update\n4. Delete")
-    choice = input('')
-    if choice == "Create":
-        def create_phone_book():
-            while True:
-                print("Click Start from editing or Exit from Exit func")
-                add = input()
-                if add == "Start":
-                    name = input("Enter name user: ")
-                    phone_number = input("Enter phone number: ")
-                    if name and phone_number:
-                        phone_book[name] = phone_number
-                    else:
-                        continue
-                elif add == "Exit":
-                    break
 
-    elif choice == "Read":
-        def read_phone_book():
-            print (phone_book.get(input("")))
-    elif choice == "Update":
-        def update_phone_book():
-            phone_book.update(input(''))
-    elif choice == "Delete":
-        def delete_user_phone():
-            del phone_book[input('')]
-else:
-    print("Make choice")
+def create_phone_book():
+    while True:
+        print("Click Start from editing or Exit from exit func")
+        add = input()
+        if add == "Start":
+            name = input("Enter name user: ")
+            phone_number = input("Enter phone number: ")
+            if name and phone_number:
+                phone_book[name] = phone_number
+            else:
+                continue
+        elif add == "Exit":
+            break
+
+
+def read_phone_book():
+    while True:
+            print("Click Start from editing or Exit from exit func")
+            add = input()
+            if add == "Start":
+                try:
+                    print(phone_book[input('')])
+                except ValueError:
+                    print("Not Search User name")
+                    continue
+            elif add == "Exit":
+                break
+
+
+def update_phone_book():
+    print(phone_book)
+    phone_book.update(input(''))
+
+
+def delete_user_phone():
+    print("Choice del element:")
+    if phone_book['']:
+        del phone_book[input('')]
+    elif print(phone_book):
+        del phone_book[input('')]
+    else:
+        print(phone_book)
+
+while True:
+    print("""Make your choice:
+    1. Create
+    2. Read
+    3. Update
+    4. Delete""")
+    choice = input("")
+    choice_menu = {'Create': create_phone_book, 'Read': read_phone_book, 'Update': update_phone_book, 'Delete': delete_user_phone}
+    choice_menu[choice]()
 
