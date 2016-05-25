929ba3ff02b0d96e94ba0f1421245a8fddc5adb7
Valerii <Valer4eg209@gmail.com>
2016-05-25 12:34:17 +0300

    Update Description.py

--- Description.py
+++ Description.py
@@ -1,10 +1,20 @@
-class Lenght:
-    def __set__(self, obj, value):
-        obj._l = value * 100
-    def __get__(self, obj, objtype):
-        return obj._l / 100
-    
+# class Lenght:
+#     def __set__(self, obj, value):
+#         obj._l = value * 100
+#     def __get__(self, obj, objtype):
+#         return obj._l / 100
+# 
+# class Line:
+#     length = Lenght()
+#     def __init__(self):
+#         self._l = 0
+        
 class Line:
-    length = Lenght()
     def __init__(self):
         self._l = 0
+    @property
+    def length(self):
+        return self._l / 100
+    @length.setter
+    def length(self, value):
+        self._l = value * 100
