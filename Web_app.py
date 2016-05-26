78f1197aa0ce90b8ccc897476eccb174c2a4f0c4
Valerii <Valer4eg209@gmail.com>
2016-05-26 15:59:16 +0300

    Update Web_app.py

--- Web_app.py
+++ Web_app.py
@@ -1,17 +1,30 @@
-from urllib import request
-
-from flask import Flask, render_template
+import redis as redis
+from flask import Flask, render_template, request, redirect, session, g
 
 app = Flask(__name__)
 
+@app.before_request
+def b_r():
+    g.r = redis.StrictRedis()
+
 @app.route('/')
 def index():
-    return render_template('Index.html')
+    name = g.r.keys('phone:*')
+    contacts = [(name[6:], g.r.get(name)) for name in name]
+    print(contacts)
+    return render_template('Index.html', message=session.pop('message'), contacts)
 
 @app.route('/<add>', methods=['GET', 'POST'])
 def add():
-    print(request.form)
-    return render_template('Add.html')
-
+    name = phone_number = ''
+    if request.method == 'POST':
+        name = request.form['name']
+        phone_number = request.form['phone_number']
+        if name and phone_number:
+            r = redis.StrictRedis()
+            r.set("phone:" + name, phone_number)
+            session['mesage'] = "Contact successfully added"
+            return redirect('/')
+    return render_template('Add.html', name=name, phone_number=phone_number)
 
 app.run(debug=True)
