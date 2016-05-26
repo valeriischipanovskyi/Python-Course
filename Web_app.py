4ae269fa48494e0eb8c4307e0ff4a9e282685551
Valerii <Valer4eg209@gmail.com>
2016-05-26 16:57:38 +0300

    Update Web_app.py

--- Web_app.py
+++ Web_app.py
@@ -1,33 +1,39 @@
-import redis as redis
+import sqlite3
 from flask import Flask, render_template, request, redirect, session, g
+
 
 app = Flask(__name__)
 
 @app.before_request
 def b_r():
-    g.r = redis.StrictRedis()
+    g.sl = sqlite3.connect('dbphone.db')
 
 @app.route('/')
 def index():
-    names = g.r.keys('phone:*')
-    contacts = [(name[6:].decode('utf8'), g.r.get(name).decode('utf8')) for name in names]
+    names = g.sl.execute('SELECT * from dbphone')
+    contacts = [name for name in names]
     print(contacts)
     return render_template('Index.html',
-                           message=session.pop('message'),
+    #                        message=session.pop('message'),
                            contacts=contacts
                            )
-
-@app.route('/<add>', methods=['GET', 'POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     name = phone_number = ''
     if request.method == 'POST':
         name = request.form['name']
         phone_number = request.form['phone_number']
         if name and phone_number:
-            r = redis.StrictRedis()
-            r.set("phone:" + name, phone_number)
+            sl = sqlite3.connect('dbphone.db')
+            sl.execute("INSERT INTO dbphone (name, Phone_number) VALUES ({});".format(name, phone_number))
             session['mesage'] = "Contact successfully added"
             return redirect('/')
     return render_template('Add.html', name=name, phone_number=phone_number)
 
+
+# @app.teardown_appcontext
+# def close_db():
+#     if hasattr(g, 'dbphone'):
+#         g.sqlite_db.close()
+app.secret_key = 'SFKHJhere7868khjh'
 app.run(debug=True)
