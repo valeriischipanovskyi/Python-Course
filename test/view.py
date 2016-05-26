6375fa6dfcf219f324df35074b29bfabcee1eb4c
Valerii <Valer4eg209@gmail.com>
2016-05-26 08:29:35 +0300

    Create view.py

---
+++ test/view.py
@@ -0,0 +1,18 @@
+def print_title():
+    print "Phones v. 1.0"
+    
+
+def get_action():
+    return raw_input("Action?")
+    
+
+def get_name():
+    return raw_input('Name?')
+
+
+def get_phone():
+    return raw_input('Phone?')
+    
+    
+def print_message(message):
+    print message
