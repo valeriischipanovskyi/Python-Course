7aa7226d8b567ad7f7227de9bb4666a5acbb82a6
Valerii <Valer4eg209@gmail.com>
2016-05-26 14:51:10 +0300

    Update PhoneBookDB.py

--- PhoneBookDB.py
+++ PhoneBookDB.py
@@ -1,28 +1,25 @@
-"""
-Create
-Read
-Update
-Delete
-"""
 import sqlite3
-import PhoneBookAbc
 
-class PhoneBook(PhoneBookAbc):
-    PHONE_BOOK_FILE = 'phone_book.txt'
+
+class PhoneBookDB:
+
+    conn = sqlite3.connect('dbphone.sqlite')
+
+    def __init__(self):
+        self.phone_book = {}
 
     def __iter__(self):
         for name, phone_number in self.phone_book.items():
             yield name, phone_number
 
-
     def load_dictionary(self):
-        self.phone_book.clear()
+        pass
 
-        file = open(PhoneBook.PHONE_BOOK_FILE, 'r')
-        for line in file.readlines():
-            name, phone_number = line.strip().split()
-            self.phone_book[name] = phone_number
-        file.close()
+
+    def create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('CREATE TABLE dbphone(name varchar(30), phone_number varchar(20))')
+        self.conn.commit()
 
     def create_phone_book(self):
         while True:
@@ -30,108 +27,59 @@ class PhoneBook(PhoneBookAbc):
                 name = input("Enter name user: ")
                 phone_number = input("Enter phone number: ")
                 if name and phone_number:
-                    with open(PhoneBook.PHONE_BOOK_FILE, 'a+t') as f:
-                        f.write("{} {}\n".format(name, phone_number))
+                    val_str = "'{}', '{}'".format(name, phone_number)
+                    sql_str = "INSERT INTO dbphone (name, Phone_number) VALUES ({});".format(val_str)
+                    self.conn.execute(sql_str)
+                    self.conn.commit()
                     print("New user? Yes/No")
                     new_user = input('')
                     if new_user == "Yes":
                         continue
                     else:
                         break
-
+                    # return self.conn.total_changes
 
     def read_phone_book(self):
-        self.load_dictionary()
-        with open(PhoneBook.PHONE_BOOK_FILE, 'a+t') as f:
-            f.read()
-        for name, phone_number in self.phone_book.items():
-            print(name, " : ", phone_number)
-            if len(self.phone_book) == 0:
-                print('Not Search User name')
+        sql_str = "SELECT * from dbphone"
+        cursor = self.conn.execute(sql_str)
+        rows = cursor.fetchall()
 
+        return print(rows)
 
     def update_phone_book(self):
-        self.load_dictionary()
-        serch = input("Enter name to search for: ")
-        if serch in self.phone_book.keys():
-            print(serch, " : ", self.phone_book[serch])
-        else:
-            print("User not found")
+        name = input("Enter name user: ")
+        phone_number = input("Enter phone number: ")
+        val_str = "'{}', '{}'".format(name, phone_number)
+        print(val_str)
+        sql_str = "UPDATE dbphone SET (name, phone_number) WHERE name={}".format(val_str)
+        self.conn.execute(sql_str)
+        self.conn.commit()
+        # return self.conn.total_changes
 
+    def delete_user_phone(self,):
+        name = input("Enter change name")
+        sql_str = "DELETE FROM dbphone WHERE name='{}'".format(name)
+        self.conn.execute(sql_str)
+        self.conn.commit()
+        # return self.conn.total_changes
 
-    def delete_user_phone(self):
-        self.load_dictionary()
-        enter_delete_user = input("Remove Name and Number: ")
-        if enter_delete_user in self.phone_book.keys():
-            del self.phone_book[enter_delete_user]
-            file = open(PhoneBook.PHONE_BOOK_FILE, 'w')
-            for name, phone_number in self.phone_book.items():
-                string = name + '\t' + phone_number + '\n'
-                file.write(string)
-            file.close()
-            print('File is delete')
-        else:
-            print("User not found")
-
-Book = PhoneBook()
+Create = PhoneBookDB()
 
 while True:
     print("""Make your choice:
+    0. Ctable
     1. Create
     2. Read
     3. Update
     4. Delete""")
     choice = input("")
-    Book.choice_menu = {'Create': Book.create_phone_book,
-                        'Read': Book.read_phone_book,
-                        'Update': Book.update_phone_book,
-                        'Delete': Book.delete_user_phone}
-    if choice not in Book.choice_menu.keys():
+    choice_menu = {'Ctable': Create.create_table,
+                   'Create': Create.create_phone_book,
+                   'Read': Create.read_phone_book,
+                   'Update': Create.update_phone_book,
+                   'Delete': Create.delete_user_phone}
+    if choice not in choice_menu.keys():
         print("Enter you choice")
     else:
-        Book.choice_menu[choice]()
+        choice_menu[choice]()
 
-class PhoneBookDB(PhoneBookAbc):
-
-    conn = sqlite3.connect('dbphone.sqlite')
-    
-    def __iter__(self):
-        for name, phone_number in self.phone_book.items():
-            yield name, phone_number
-
-
-
-    def create_table(self):
-        cursor = self.conn.cursor()
-        cursor.execute('CREATE TABLE dbphone("name varchar(30)", "phone_number varchar(20)"')
-        self.conn.commit()
-
-
-    def create_phone_book(self, name, phone_number):
-        val_str = "'{}', '{}'".format(name, phone_number)
-        sql_str = "INSERT INTO phone_book (name, Phone_number) VALUES ({});".format(val_str)
-        self.conn.execute(sql_str)
-        self.conn.commit()
-        return self.conn.total_changes
-
-    def read_phone_book(self):
-        sql_str = "SELECT * from PHONE_BOOK"
-        cursor = self.conn.execute(sql_str)
-        rows = cursor.fetchall()
-        return rows
-
-    def update_phone_book(self, name, phone_number):
-        val_str = "name='{}', Phone Number={}".format(name, phone_number)
-        sql_str = "Update Phone Book set {} where name= ;".format(val_str)
-        self.conn.execute(sql_str)
-        self.conn.commit()
-        return self.conn.total_changes
-
-    def delete_user_phone(self, change_name):
-        sql_str = "DELETE from PHONE_BOOK where Name={}".format(change_name)
-        self.conn.execute(sql_str)
-        self.conn.commit()
-        return self.conn.total_changes
-
-Create = PhoneBookDB()
-# create_table()
